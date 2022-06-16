87969965c6cede4895514dabec55b93c46fccbee
yair sabag <yairsabag213@gmail.com>
2022-06-16 12:26:42 +0300

    add .env to .gitignore

--- src/api/resume.js
+++ src/api/resume.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/';
+const API_URL = 'http://localhost:4000/';
 
 const API = axios.create({
     baseURL: API_URL
--- src/missions.js
+++ src/missions.js
@@ -1,6 +1,8 @@
 /*
-1. display all resumes in diferent route 
+1. display all resumes in diferent route "/allResume"
 
+2. diplay single resume by id /"[id]" in diferent route
 
+3. render as pdf file the resumes
 
 */
