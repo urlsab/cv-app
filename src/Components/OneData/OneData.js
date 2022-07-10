a095cccab42371e2bcdb45df0a6167cbf3b4d6b6
yair sabag <yairsabag213@gmail.com>
2022-07-10 10:16:26 +0300

    add logic of pdf function

--- src/Components/OneData/OneData.js
+++ src/Components/OneData/OneData.js
@@ -1,10 +1,37 @@
 import React from 'react';
-//import { InitialState } from '../../InitialState/InitialState';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
-const OneData = () => (
-  <div>
-    <p>piece of data</p>
-  </div>
-);
+//import { useRef } from 'react';
+
+const OneData = () => {
+
+  const printRef = React.useRef();
+
+  const handleDownloadPdf = async () => {
+    const element = printRef.current;
+    const canvas = await html2canvas(element);
+    const data = canvas.toDataURL('image/png');
+
+    const pdf = new jsPDF();
+    const imgProperties = pdf.getImageProperties(data);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight =
+      (imgProperties.height * pdfWidth) / imgProperties.width;
+
+    pdf.addImage(data, 'PNG', 0, 0, pdfWidth, pdfHeight);
+    pdf.save('print.pdf');
+  };
+
+  return (
+    <div>
+      <p>piece of data</p>
+      <button type="button" onClick={handleDownloadPdf}>
+        Download as PDF
+      </button>
+      <div ref={printRef}>I will be in the PDF.</div>
+    </div>
+  )
+};
 
 export default OneData;
